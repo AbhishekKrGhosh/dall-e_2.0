9f331cb333093915a579334f47de8fe3f175d237
Abhishek Kumar <abhishek.andri05@gmail.com>
2023-11-01 10:13:40 +0530

    createPost done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ const RenderCards = ({ data, title })=>{
     return data.map((post)=><Card key={post._id} {...post}/>)
   }
   return(
-    <h2 className='mt-5 font-bold text-[#6469ff] text-xl uppercase'>{title}</h2>
+    <h2 className='mt-5 font-bold text-[#4649ff] text-xl uppercase'>{title}</h2>
   )
 }
 
